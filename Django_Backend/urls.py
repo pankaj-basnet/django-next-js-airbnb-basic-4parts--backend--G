464a8e2eddbe1107068faedc2d9428c1ce28b04c
Pankaj Basnet <pankajbasnet2020@hotmail.com>
2024-08-02 18:12:17 +0545

    part-4--stein--done-------image only not seen of propertyListItem

--- Django_Backend/urls.py
+++ Django_Backend/urls.py
@@ -24,5 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/properties/', include('property.urls')),
+    path('api/auth/', include('useraccount.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
